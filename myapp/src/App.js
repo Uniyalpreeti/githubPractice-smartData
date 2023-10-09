6cbb377b22493595f95f65c2a23786d989ff27c0
Uniyalpreeti <Preeti20uniyal@gmail.com>
2023-10-09 10:15:58 +0530

    Fifth Branch m changes kie gye hai

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div>
      <h1><Comp1 /></h1>
      //Something went wrong on this project.
-     //This is the fifth branch and i am updating here to see the changes in this branch and push the changes in this branch.
+     //Yha pr kie gye changes fifth branch m hme or branches m b show krwane hai.
     </div>
   );
 }
